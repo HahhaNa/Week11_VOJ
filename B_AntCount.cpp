1653e0c0e40b4b691782790c2935e1168c3b2b94
Hanna <hihahanaisme@gmail.com>
2024-03-02 17:37:57 +0800

    math method (WA when left, right > N)

--- B_AntCount.cpp
+++ B_AntCount.cpp
@@ -1,36 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int dp[1005];
 
 int main() {
     int types, nums, left, right;
     cin >> types >> nums >> left >> right;
-    int count[1005]; // count[i] = x -> i repeat x times
-    int N[100005]; // N[i] = x -> x types are repeated i times (or more)
-    N[0] = 0;
+    int count[1005] = {0}; // count[i]=x -> i repeat x times
+    int N[100005] = {0};
     for (int i = 0; i < nums; i++) {       
         int n; cin >> n;
-        count[n] += 1;
-        N[count[n]] += 1;
+        count[n]++;
+        N[count[n]]++;
     }
 
-    int ans = 0;
-    dp[0] = 1;
-    for(int i=1; i<=right; i++) {
-        if(i==1) dp[i] = N[i];
-        else if(i==2) dp[i] = types*(types-1)/2 + N[2];
-        else if(i < (nums+1)/2) {
-            dp[i] = C(types, i) + N[i-1]*(types-1) + N[i];
-        } 
-        else {
-            dp[i] = dp[nums-i];
-        }
-        if(i>=left && i<=right) ans += dp[i];
-        ans %= 1000000;
+    long long ans = 1;
+    int up = types+right-left;
+    int down = right+1;
+    for(int i = up; i >= down; i--) {
+        ans *= i;
     } 
-    // for(int i=0; i<=nums; i++)
-    //     cout << "in " << i << " dp: " << dp[i] << endl;
+    ans = (ans+1000000) % 1000000;
+    for (int i = 1; i <= up-down; i++) {
+        ans /= i;
+    }
+    ans = (ans+1000000) % 1000000;
+    if(left != right) {
+        for(int i=left; i<right; i++) {
+            ans += types*(N[i]);
+        }   ans += N[right];    
+    } else {
+        ans += (types-1)*N[left];
+    }
+    
+    ans = (ans+1000000) % 1000000;
+    
     cout << ans << endl;
     return 0;
 }
