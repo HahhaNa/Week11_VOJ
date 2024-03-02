90274cfd77c070c2b3659ae0bf4801cc1b1a6d9c
Hanna <hihahanaisme@gmail.com>
2024-03-02 23:33:33 +0800

    AC with online solution

--- B_AntCount.cpp
+++ B_AntCount.cpp
@@ -1,39 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int dp[2][100002] = {0};
 
 int main() {
-    int types, nums, left, right;
-    cin >> types >> nums >> left >> right;
+    int types, nums, lower, upper;
+    cin >> types >> nums >> lower >> upper;
     int count[1005] = {0}; // count[i]=x -> i repeat x times
-    int N[100005] = {0};
     for (int i = 0; i < nums; i++) {       
         int n; cin >> n;
         count[n]++;
-        N[count[n]]++;
     }
-
-    long long ans = 1;
-    int up = types+right-left;
-    int down = right+1;
-    for(int i = up; i >= down; i--) {
-        ans *= i;
-    } 
-    ans = (ans+1000000) % 1000000;
-    for (int i = 1; i <= up-down; i++) {
-        ans /= i;
+    dp[0][0] = dp[1][0] = 1;
+    for (int i = 1; i <= types; i++) {
+        for (int j = 1; j <= upper; j++) {
+            if(j-count[i] >= 1)
+                dp[i%2][j] = (dp[(i-1)%2][j] + dp[i%2][j-1] - dp[(i-1)%2][j-count[i]-1]+1000000) % 1000000;
+            else dp[i%2][j] = dp[(i-1)%2][j] + dp[i%2][j-1];
+            dp[i%2][j] = dp[i%2][j] % 1000000;
+        }
     }
-    ans = (ans+1000000) % 1000000;
-    if(left != right) {
-        for(int i=left; i<right; i++) {
-            ans += types*(N[i]);
-        }   ans += N[right];    
-    } else {
-        ans += (types-1)*N[left];
+    int ans = 0;
+    for(int i = lower; i <= upper; i++) {
+        ans = (ans + dp[types%2][i]) % 1000000;
     }
-    
-    ans = (ans+1000000) % 1000000;
-    
     cout << ans << endl;
     return 0;
 }
